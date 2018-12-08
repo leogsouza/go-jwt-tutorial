f05d3d9513befb712bb11e2d1d6b19a5978275fd
Leonardo Souza <leogsouza@gmail.com>
2018-12-08 00:12:20 -0200

    Handle client requests

--- client/main.go
+++ client/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 var mySigningKey = []byte("MySecredtKey")
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, validToken)
+}
 
 func GenerateJWT() (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -30,14 +41,13 @@ func GenerateJWT() (string, error) {
 
 }
 
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
 func main() {
 	fmt.Println("My Simple Client")
 
-	tokenString, err := GenerateJWT()
-
-	if err != nil {
-		fmt.Println("error generating token string")
-	}
-
-	fmt.Println(tokenString)
+	handleRequests()
 }
